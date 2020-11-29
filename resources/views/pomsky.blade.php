b7724c8dfcb5939cb3269b8bd8c37cca7cd4c894
Rugie Ann Barrameda <norugie015@gmail.com>
2020-11-28 21:51:16 -0800

    added layout for about

--- resources/views/pomsky.blade.php
+++ resources/views/pomsky.blade.php
@@ -1,5 +1,180 @@
 @extends ( 'layouts.layout' )
 
 @section ( 'content' )
-pomsky page
+<!-- Full Width Banner Parallax Section -->
+<div class="section-block featured-media page-intro tm-slider-parallax-container">
+	<div class="tm-slider-container full-width-slider" data-parallax data-parallax-fade-out data-animation="slide" data-scale-under="960">
+		<ul class="tms-slides">
+			<li class="tms-slide" data-image data-force-fit data-overlay-bkg-color="#333333" data-overlay-bkg-opacity="0.6">
+				<div class="tms-content">
+					<div class="tms-content-inner center">
+						<div class="row flex">
+							<div class="column width-6 offset-3">
+								<div>
+									<h1 class="tms-caption mb-0 color-white"
+										data-animate-in="preset:slideInUpShort;duration:1000ms;delay:100ms;"
+										data-no-scale
+									>What is a Pomsky?</h1>
+									<div class="clear"></div>
+									<p class="tms-caption lead mb-0 mb-mobile-20 color-white opacity-07"
+										data-animate-in="preset:slideInUpShort;duration:1000ms;delay:100ms;"
+										data-no-scale
+									>Cute and silly - a designer breed</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<img data-src="images/assets/about/pomsky-about-bg.jpg" data-retina src="images/blank.png" alt=""/>
+			</li>
+		</ul>
+	</div>
+</div>
+<!-- Full Width Banner Parallax Section -->
+
+<!-- About Pomsky Section -->
+<div class="section-block replicable-content">
+	<div class="row">
+		<div class="column width-12">
+			<div class="row">
+				<div class="column width-12">
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<p class="lead">Velit veniam consequat anim duis culpa enim cupidatat ullamco ut et dolore nisi aute. Ipsum culpa adipisicing aliqua reprehenderit quis ea ipsum irure id. Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Our Ideal Pomsky</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Temperament</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+					<span class="progress-bar-label">Temperament 100%</span>
+					<div class="progress-bar rounded small bkg-white tmh-perspective">
+						<div class="bar bkg-blue-light color-white percent-100" data-animate-in="transX:-100%;duration:1000ms;easing:easeIn;" style="backface-visibility: hidden; transition: -webkit-transform 1000ms ease 0s, opacity; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1); opacity: 1; visibility: visible; transform-origin: 50% 50% 0px;">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Intelligence</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+					<span class="progress-bar-label">Intelligence 100%</span>
+					<div class="progress-bar rounded small bkg-white tmh-perspective">
+						<div class="bar bkg-blue-light color-white percent-100 horizon" data-animate-in="transX:-100%;duration:1000ms;easing:easeIn;" style="backface-visibility: hidden; transition: -webkit-transform 1000ms ease 0s, opacity; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1); opacity: 1; visibility: visible; transform-origin: 50% 50% 0px;">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Energy Level</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+					<span class="progress-bar-label">Energy Level 70%</span>
+					<div class="progress-bar rounded small bkg-white tmh-perspective">
+						<div class="bar bkg-blue-light color-white percent-70 horizon" data-animate-in="transX:-100%;duration:1000ms;easing:easeIn;" style="backface-visibility: hidden; transition: -webkit-transform 1000ms ease 0s, opacity; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1); opacity: 1; visibility: visible; transform-origin: 50% 50% 0px;">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Markings and Colour</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+					<div class="grid-container fade-in-progressively no-padding-top" data-grid-ratio="1" data-animate-resize="" data-animate-resize-duration="0">
+						<div class="row">
+							<div class="column width-12">
+								<div class="row grid content-grid-3" style="position: relative; height: 450px; visibility: visible; min-height: initial;">
+									<div class="grid-item animate-in" style="position: absolute; left: 0px; top: 0px;">
+										<div class="thumbnail rounded img-scale-in" data-hover-easing="easeInOut" data-hover-speed="500">
+											<a class="overlay-link lightbox-link" data-toolbar="zoom" data-group="pomsky-marking-1" data-image-url="images/assets/about/pomsky-marking-1.jpg" href="images/assets/about/pomsky-marking-1.jpg" data-lightbox-animation="fadeIn" id="pomsky-marking-1-tml-thumb-1">
+												<img src="images/assets/about/pomsky-marking-1.jpg" alt="" style="transition-duration: 500ms;">
+											</a>
+										</div>
+									</div>
+									<div class="grid-item animate-in" style="position: absolute; left: 379px; top: 0px;">
+										<div class="thumbnail rounded img-scale-in" data-hover-easing="easeInOut" data-hover-speed="700">
+											<a class="overlay-link lightbox-link" data-toolbar="zoom" data-group="pomsky-marking-1" data-image-url="images/assets/about/pomsky-marking-2.jpg" href="images/assets/about/pomsky-marking-2.jpg" data-lightbox-animation="fadeIn" id="pomsky-marking-1-tml-thumb-2">
+												<img src="images/assets/about/pomsky-marking-2.jpg" alt="" style="transition-duration: 700ms;">
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Eye Colour</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+					<div class="grid-container fade-in-progressively no-padding-top" data-grid-ratio="1" data-animate-resize="" data-animate-resize-duration="0">
+						<div class="row">
+							<div class="column width-12">
+								<div class="row grid content-grid-3" style="position: relative; height: 450px; visibility: visible; min-height: initial;">
+									<div class="grid-item animate-in" style="position: absolute; left: 0px; top: 0px;">
+										<div class="thumbnail rounded img-scale-in" data-hover-easing="easeInOut" data-hover-speed="500">
+											<a class="overlay-link lightbox-link" data-toolbar="zoom" data-group="pomsky-eye-1" data-image-url="images/assets/about/pomsky-eye-1.jpg" href="images/assets/about/pomsky-eye-1.jpg" data-lightbox-animation="fadeIn" id="pomsky-eye-1-tml-thumb-1">
+												<img src="images/assets/about/pomsky-eye-1.jpg" alt="" style="transition-duration: 500ms;">
+											</a>
+										</div>
+									</div>
+									<div class="grid-item animate-in" style="position: absolute; left: 379px; top: 0px;">
+										<div class="thumbnail rounded img-scale-in" data-hover-easing="easeInOut" data-hover-speed="700">
+											<a class="overlay-link lightbox-link" data-toolbar="zoom" data-group="pomsky-eye-1" data-image-url="images/assets/about/pomsky-eye-2.jpg" href="images/assets/about/pomsky-eye-2.jpg" data-lightbox-animation="fadeIn" id="pomsky-eye-1-tml-thumb-2">
+												<img src="images/assets/about/pomsky-eye-2.jpg" alt="" style="transition-duration: 700ms;">
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Fur and Coat</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+					<span class="progress-bar-label">Fur and Coat: Low Maintenance 30%</span>
+					<div class="progress-bar rounded small bkg-white tmh-perspective">
+						<div class="bar bkg-blue-light color-white percent-30 horizon" data-animate-in="transX:-100%;duration:1000ms;easing:easeIn;" style="backface-visibility: hidden; transition: -webkit-transform 1000ms ease 0s, opacity; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1); opacity: 1; visibility: visible; transform-origin: 50% 50% 0px;">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="column width-12">
+					<h3 class="post-title">Size and Weight</h3>
+					<p class="lead">Eu nostrud minim voluptate aliquip. Nulla aute adipisicing sit duis eu eiusmod voluptate officia adipisicing minim culpa ea et do. Quis minim mollit elit esse do esse laborum ea officia. Fugiat exercitation voluptate occaecat dolore amet non ea velit non excepteur Lorem. Amet eu Lorem duis ullamco reprehenderit.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- About Pomsky Section End -->
+
+<!-- About Pomsky Ad Split Section -->
+<div class="section-block pt-40 pb-40 bkg-blue-light">
+	<div class="row flex tmh-perspective">
+		<div class="column width-8 left">
+			<p class="lead mb-0 mb-mobile-20 color-white">Want a pomsky? Check out our "Available Puppies" section now.</p>
+		</div>
+		<div class="column width-4 v-align-middle right left-on-mobile ">
+			<div>
+				<a href="/available-puppies" class="button rounded medium no-margins bkg-white bkg-hover-white color-blue-light color-hover-blue-light">Available Puppies</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- About Pomsky Ad Split Section End -->
 @endsection
