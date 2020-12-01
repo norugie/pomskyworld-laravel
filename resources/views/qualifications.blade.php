9dc4b55937658711fc1a3a50d775b7ed24f3c1ca
Rugie Ann Barrameda <norugie015@gmail.com>
2020-11-30 20:15:08 -0800

    qualifications layout

--- resources/views/qualifications.blade.php
+++ resources/views/qualifications.blade.php
@@ -1,5 +1,37 @@
 @extends ( 'layouts.layout' )
 
 @section ( 'content' )
-qualifications page
+<!-- Pomskyworld Pricing Section -->
+<div class="section-block replicable-content">
+	<div class="row">
+        <div class="column width-12 center">
+            <h3 class="post-title">Adoption Qualifications</h3>
+            <p class="lead mb-50">Duis veniam proident tempor anim id fugiat. Aliquip deserunt ipsum cillum aliqua amet et exercitation Lorem. Enim reprehenderit esse est officia labore et laborum non et elit dolore incididunt incididunt.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column width-12">
+            <p class="announcement rounded bkg-blue-light left mb-50 color-white center post-title lead">Questions to ask yourself when adopting a Pomsky</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column width-12">
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Which generation of Pomsky are you interested in?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;What colour and gender of Pomsky are you interested in?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Is there a specific puppy from a litter that you are interested in?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;How active is your life style?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;How often will you exercise your Pomsky?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Are dogs allowed in your renting agreement? (We require proof from your landlord that they accept pets.)</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Do you plan to offer continued veterinary medical care for your Pomsky on a yearly health check basis?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;What research have you done on raising a puppy?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Are you aware of the average yearly cost of keeping a dog?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Do you plan to train your Pomsky to enjoy the sports of Dog Agility, Flyball and Obedience?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Would you ever consider surrendering your Pomsky to a rescue organization should you no longer be able to care for him/her?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Have you read through our Health Guarantee section of the website?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Are you confident that you are not allergic to dogs, including their dander and saliva?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Has everyone in your family agreed upon the arrival of a new puppy?</p>
+            <p class="lead"><span class="icon-check medium border-blue-light thick color-blue-light"></span>&nbsp;&nbsp;Do you plan on taking any puppy training/socialization classes with your Pomsky?</p>
+        </div>
+    </div>
+</div>
 @endsection
